cfa161d894236a7a3b17dabd68defd4f313b978f
SittikronNon <sittikron.lamulsab@gmail.com>
2023-11-11 23:17:50 +0700

    HTML markup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import Home from './components/Home';
+import React from 'react';
+import Main from './components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Home />
-      </div>
-    </div>
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
   );
 }
 
